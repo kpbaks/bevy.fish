e9ff1b28365940f5bed3946ba563f097fb622e44
kpbaks <kristoffer.pbs@gmail.com>
2024-03-26 14:34:31 +0100

    feat: add `bevy-states`

--- conf.d/bevy.fish
+++ conf.d/bevy.fish
@@ -3,3 +3,4 @@ abbr -a bvc bevy-components
 abbr -a bve bevy-events
 abbr -a bvp bevy-plugins
 abbr -a bvr bevy-resources
+abbr -a bvs bevy-states
---
+++ functions/bevy-states.fish
@@ -0,0 +1,43 @@
+function bevy-states -d ''
+    set -l options h/help
+    if not argparse $options -- $argv
+        printf '\n'
+        eval (status function) --help
+        return 2
+    end
+
+    if set --query _flag_help
+        set -l option_color (set_color $fish_color_option)
+        set -l reset (set_color normal)
+        set -l bold (set_color --bold)
+        set -l section_header_color (set_color yellow)
+
+        printf '%sdescription%s\n' $bold $reset
+        printf '\n'
+        printf '%sUSAGE:%s %s%s%s [OPTIONS]\n' $section_header_color $reset (set_color $fish_color_command) (status function) $reset
+        printf '\n'
+        printf '%sOPTIONS:%s\n' $section_header_color $reset
+        printf '%s\t%s-h%s, %s--help%s Show this help message and return\n'
+        # printf '%sEXAMPLES:%s\n' $section_header_color $reset
+        # printf '\t%s%s\n' (printf (echo "$(status function)" | fish_indent --ansi)) $reset
+        return 0
+    end >&2
+
+    if not command --query rg
+        return 1
+    end
+
+    if test $PWD = $HOME
+        return 2
+    end
+
+    if not test -f Cargo.toml
+        return 2
+    end
+
+    set -l rg_opts --pretty --smart-case --pcre2-unicode -A 1
+    set -l re '^#\[derive\(.*States.*\)\]'
+    command rg $rg_opts "$re"
+
+    return 0
+end
